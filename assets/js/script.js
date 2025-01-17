13ad1309dff6e46604261fe54bc4e574e4609fd1
Sacaia <30930768+sacaia@users.noreply.github.com>
2025-01-16 23:34:26 -0300

    Visual

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,27 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const inputOutputList = [
-    { input: 'hello', output: 'world' },
-    { input: 'foo', output: 'bar' },
-    { input: 'test', output: 'success' },
+    { input: '123', output: 'Voce acha que eu sou otário?' },
+    { input: '321', output: '?' },
+    { input: '404', output: 'Error' },
     // Add more input-output pairs as needed
   ];
 
   function processInput() {
     const userInput = document.getElementById('userInput').value;
-    let result = 'Error: No matching input found';
+    let result = 'Você jamais passará do meu sistema de segurança gru!';
+    let resultClass = 'text-danger';
 
     for (let pair of inputOutputList) {
       if (pair.input === userInput) {
         result = pair.output;
+        resultClass = 'text-success';
         break;
       }
     }
 
-    document.getElementById('result').innerText = 'Processed result: ' + result;
+    const resultElement = document.getElementById('result');
+    resultElement.innerText = result;
+    resultElement.className = 'center-text title-margin ' + resultClass;
   }
 
   document.querySelector('button').addEventListener('click', processInput);
