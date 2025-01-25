72f0d287e1c3fdafc1e23f0e1905e05a2c8ca769
Sacaia <30930768+sacaia@users.noreply.github.com>
2025-01-24 22:22:22 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,20 +13,20 @@ document.addEventListener('DOMContentLoaded', function () {
     { input: 'TUVVNWCTYUOU', output: 'Um passeio no bosque' },
     { input: 'TUUCPTTTGDXP', output: 'Procure na cozinha pelo café' },
     { input: 'YVUYPTPDWHQX', output: 'uma área aberta, geralmente localizada na parte externa de uma construção, como uma casa ou apartamento, que pode ser coberta ou descoberta' },
-    { input: '3312', output: 'Voce acha que eu sou otário?' },
-    { input: '7552', output: '?' },
-    { input: '1742', output: 'Error' },
+    { input: '3312', output: 'Vocês destravaram a escada para subir até a sala da fortaleza de Vector!' },
+    { input: '7552', output: 'Vocês destravaram a escada para subir até o segundo piso da fortaleza de Vector!' },
+    { input: '1742', output: 'Vocês destravaram a escada para subir até o ultimo piso da fortaleza de Vector!' },
     // Add more input-output pairs as needed
   ];
 
   function processInput() {
-    const userInput = document.getElementById('userInput').value;
+    const userInput = document.getElementById('userInput').value.trim().toUpperCase(); 
     let result = 'Você jamais passará do meu sistema de segurança gru!';
     let resultClass = 'text-danger';
     let showErrorImage = true;
 
     for (let pair of inputOutputList) {
-      if (pair.input === userInput.toUpperCase()) {
+      if (pair.input === userInput) {
         result = pair.output;
         resultClass = 'text-success';
         showErrorImage = false;
